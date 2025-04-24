475ec16f50a9a120bfde091d129b6d6ac5212fda
Angela Venable <avenable11@ivytech.edu>
2025-04-24 00:11:14 +0000

    with lecture activity sockets

--- 04_23_25/client.cpp
+++ 04_23_25/client.cpp
@@ -1,6 +1,9 @@
 #include "PracticalSocket.h"
+#include "suit.h"
 #include <iostream>
 #include <limits>
+#include <map>
+#include <string>
 
 void resetStream();
 
@@ -11,26 +14,55 @@ int main(int argc, char *argv[])
         std::cerr << "Usage: ./client <server ip address>" << std::endl;
         return 1;
     }
+    std::map<suitType, std::string> suitSymbols;
+    suitSymbols[suitType::CLUBS] = "♣";
+    suitSymbols[suitType::DIAMONDS] = "♦";
+    suitSymbols[suitType::HEARTS] = "♥";
+    suitSymbols[suitType::SPADES] = "♠";
+    std::map<int, suitType> suitNumbers;
+    suitNumbers[1] = suitType::HEARTS;
+    suitNumbers[2] = suitType::SPADES;
+    suitNumbers[3] = suitType::CLUBS;
+    suitNumbers[4] = suitType::DIAMONDS;
 
     try
     {
         TCPSocket sock(argv[1], 9431);
         int input = 0;
-        std::cout << "Enter the starting number: ";
-        std::cin >> input;
-        while (!std::cin)
+        int i = 1;
+        std::cout << "Pick a suit: " << std::endl;
+        for (auto it = suitSymbols.begin(); it != suitSymbols.end(); ++it)
         {
-            resetStream();
-            std::cout << "Enter the starting number: ";
+            std::cout << i++ << ": " << it->second << std::endl;
+        }
+        std::cin >> input;
+        while (!std::cin || input < 1 || input > 4)
+        {
+            if (!std::cin)
+            {
+                resetStream();
+            }
+            i = 1;
+            std::cout << "Pick a suit: " << std::endl;
+            for (auto it = suitSymbols.begin(); it != suitSymbols.end(); ++it)
+            {
+                std::cout << i++ << ": " << it->second << std::endl;
+            }
             std::cin >> input;
         }
-        uint32_t val = static_cast<uint32_t>(input);
+        suitType suit = suitNumbers[input];
+        uint32_t val = static_cast<uint32_t>(suit);
         val = htonl(val);
         sock.send(&val, sizeof(val));
         if (sock.recvFully(&val, sizeof(val)) == sizeof(val))
         {
             val = ntohl(val);
-            std::cout << "Server Response: " << val << std::endl;
+            char *buffer = new char[val + 1];
+            if (sock.recvFully(buffer, val) == val)
+            {
+                buffer[val] = '\0';
+                std::cout << "Server Response: " << buffer << suitSymbols[suit] << std::endl;
+            }
         }
         sock.close();
     }
--- 04_23_25/server.cpp
+++ 04_23_25/server.cpp
@@ -1,5 +1,11 @@
 #include "PracticalSocket.h"
+#include "suit.h"
+#include <string>
 #include <iostream>
+#include <map>
+// lecture activity part b change the suit colors
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+std::string getFormatStr(suitType suit);
 
 int main(int argc, char *argv[])
 {
@@ -12,11 +18,20 @@ int main(int argc, char *argv[])
         {
             std::cout << "incoming client: " << std::endl;
             val = ntohl(val);
-            std::cout << val << std::endl;
-            val++;
-            val = htonl(val);
+            std::cout << "receiving " << val << std::endl;
+            suitType suit = static_cast<suitType>(val);
+            std::string response = getFormatStr(suit);
+            val = htonl(response.length());
             sock->send(&val, sizeof(val));
+            sock->send(response.c_str(), response.length());
         }
         delete sock;
     }
+}
+
+std::string getFormatStr(suitType suit)
+{
+    static std::map<suitType, std::string> suitColors = {{suitType::HEARTS, "\033[1m\033[107;31m"}, {suitType::DIAMONDS, "\033[1m\033[107;31m"}, {suitType::CLUBS, "\033[1m\033[107;30m"}, {suitType::SPADES, "\033[1m\033[107;30m"}};
+
+    return suitColors[suit];
 }
